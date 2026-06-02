f60a9eedeafea533965b39ac7d89308db3dec756
Rithanya CS <rithanyacs07@gmail.com>
2026-06-02 10:28:34 +0530

    Add files via upload

---
+++ graphs/Animal.java
@@ -0,0 +1,17 @@
+class Animal{
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("barking...");
+    }
+
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.bark();
+    }
+}
